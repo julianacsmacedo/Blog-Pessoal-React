68d681252e7ca88ede252bfb078e4b1db648ab49
julianacsmacedo <julianamacedo2008@gmail.com>
2021-11-26 18:32:31 -0300

    add react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+        //caminho abreviado
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/msT7EyD.jpeg" alt="Imagem do Horizonte" className="img" />
+        </>
+    );
+}
+
+export default Home;
