7862e7535782a5bffa448cc36cd4c3b80a636320
julianacsmacedo <julianamacedo2008@gmail.com>
2021-12-01 21:48:05 -0300

    add cadastro

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -6,20 +6,23 @@ import { Link } from 'react-router-dom';
 function CadastroUsuario() {
 
     return (
-        <Grid container direction='row' justifyContent='center' alignItems='center' className='imagem2 rotação-img'>
-            <Grid item xs={6} ></Grid>
+        <Grid container direction='row' justifyContent='center' alignItems='center' className='back'>
+            <Grid item xs={6} className='imagem2'></Grid>
             <Grid item xs={6} alignItems='center'>
-            <Box>
+            <Box paddingX={10}>
                 <form>
-                    <Typography variant="h3" gutterBottom align="center" component="h3" className="texto1">Entrar</Typography>
+                    <Typography variant="h3" gutterBottom align="center" component="h3" className="texto2">Cadastrar</Typography>
+                    <TextField id="nome" label="nome" variant="outlined" name="nome" margin="normal" fullWidth/>
                     <TextField id="usuario" label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
                     <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
-                    <TextField id="usuario" label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
-                    <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
+                    <TextField id="confirmarSenha" label="confirmarSenha" variant="outlined" name="confirmarSenha" margin="normal" type="password" fullWidth />
                     <Box marginTop={2} textAlign='center'>
-                        <Link to='/home' className='text-decorator-none'>
-                            <Button type='submit' variant='contained' className="botao">
-                                Logar
+                        <Link to='/login' className='text-decorator-none'>
+                            <Button variant='contained' color="secondary" className='btnCancelar'>
+                                Cancelar
+                            </Button>
+                            <Button type='submit' variant='contained' color="primary">
+                                Cadastrar
                             </Button>
                         </Link>
                     </Box>
