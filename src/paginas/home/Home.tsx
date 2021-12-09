8eeec8bdde30d1ca4cde4d84f702da2774457438
julianacsmacedo <julianamacedo2008@gmail.com>
2021-12-09 10:58:58 -0300

    add Modal

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Home.css';
-import {Grid, Box, Typography, Button} from '@material-ui/core';
+import { Grid, Box, Typography, Button } from '@material-ui/core';
 import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+import { useHistory } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 
 function Home() {
-    return(
+
+    let history = useHistory();
+    const [token, setToken] = useLocalStorage('token');
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+            history.push("/login")
+
+        }
+    }, [token])
+
+    return (
         //caminho abreviado
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" className="background back">
@@ -15,6 +30,7 @@ function Home() {
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
+                            <ModalPostagem />
                         </Box>
                         <Button variant="outlined" className="botao">Ver Postagens</Button>
                     </Box>
@@ -22,8 +38,10 @@ function Home() {
                 <Grid item xs={6} className="back">
                 </Grid>
 
-                <Grid xs={12} className="postagens">
-                < TabPostagem />
+                <Grid alignItems="center" item xs={12} className="postagens">
+                    <Box >
+                    < TabPostagem />
+                    </Box>
                 </Grid>
             </Grid>
         </>
